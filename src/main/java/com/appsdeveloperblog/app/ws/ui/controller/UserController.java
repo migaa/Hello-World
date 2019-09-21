7dcd5e561205167d365122767982d95717ce65d5
migaats <tsmigaa@gmail.com>
2019-09-21 19:30:58 +0800

    test

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -2,19 +2,29 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("/users") //http://localhost:8080/users
 public class UserController {
 
 	@GetMapping
-	public String getUser()
+	public String getUsers(@RequestParam(value="page", defaultValue="1") int page,
+						   @RequestParam(value="limit", defaultValue="50") int limit,
+						   @RequestParam(value="sort", defaultValue="desc", required=false) String sort)
 	{
-		return "get user was called";
+		return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+	}
+	
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId)
+	{
+		return "get user was called with userId = " + userId;
 	}
 	
 	@PostMapping
