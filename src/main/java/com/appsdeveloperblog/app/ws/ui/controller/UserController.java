2c2f46fc573bdff2014c17252e92c5a0a54dd38a
migaats <tsmigaa@gmail.com>
2019-09-21 21:55:57 +0800

    Section 6 complete

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users") //http://localhost:8080/users
@@ -21,10 +26,19 @@ public class UserController {
 		return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
 	}
 	
-	@GetMapping(path="/{userId}")
-	public String getUser(@PathVariable String userId)
+	@GetMapping(path="/{userId}", 
+			produces = { 
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE 
+					})
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId)
 	{
-		return "get user was called with userId = " + userId;
+		UserRest returnValue = new UserRest();
+		returnValue.setFirstName("Myagmardorj");
+		returnValue.setLastName("Ts.");
+		returnValue.setEmail("tsmigaa@gmail.com");
+		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
 	@PostMapping
