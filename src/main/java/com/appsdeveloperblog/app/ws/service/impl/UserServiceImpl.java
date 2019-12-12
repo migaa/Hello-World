23a1c11d903089d24b8b00dbdc09aeeafcfb44a0
Myagmardorj Tsogtbayar <myagmardorj.ts@ckbank.mn>
2019-12-12 16:58:56 +0800

    Lesson20-24 done. must email verification on AWS

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -19,6 +19,7 @@ import com.appsdeveloperblog.app.ws.exceptions.UserServiceException;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.io.repositories.UserRepository;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.AmazonSES;
 import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
@@ -70,6 +71,10 @@ public class UserServiceImpl implements UserService {
 		//BeanUtils.copyProperties(storedUserDetails, returnValue);
 		UserDto returnValue = modelMapper.map(storedUserDetails, UserDto.class);
 
+		// Send an email message to user to verify their email address
+		new AmazonSES().verifyEmail(returnValue);
+		
+		
 		return returnValue;
 	}
 
